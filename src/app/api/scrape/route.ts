30ef72a48e26e728aeeee67a5f049cc4640de730
rivas-kdt <rivas-kdt@global.kawasaki.com>
2025-10-14 13:24:48 +0800

    test

--- src/app/api/scrape/route.ts
+++ src/app/api/scrape/route.ts
@@ -1,12 +1,14 @@
-import chromium from "chrome-aws-lambda";
+import chromium from "@sparticuz/chromium";
 import puppeteer from "puppeteer-core";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  const executablePath = await chromium.executablePath();
+
   const browser = await puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath,
+    executablePath,
     headless: chromium.headless,
   });
 
@@ -15,10 +17,10 @@ export async function GET() {
     waitUntil: "networkidle2",
   });
 
-  await page.waitForSelector("#related #items");
-  const relatedHTML = await page.$eval("#related #items", (el) => el.innerHTML);
+  await page.waitForSelector("#related #items", { timeout: 10000 });
+  const html = await page.$eval("#related #items", (el) => el.innerHTML);
 
   await browser.close();
 
-  return NextResponse.json({ success: true, relatedHTML });
+  return NextResponse.json({ success: true, html });
 }
