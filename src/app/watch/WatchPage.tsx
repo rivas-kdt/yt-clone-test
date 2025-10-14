268aed5ce5444cb8b20cce0b68241d9d37c301fa
rivas-kdt <rivas-kdt@global.kawasaki.com>
2025-10-14 10:35:57 +0800

    Resizer

--- src/app/watch/WatchPage.tsx
+++ src/app/watch/WatchPage.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import YouTube from "react-youtube";
 
 export default function WatchPage() {
   const searchParams = useSearchParams();
   const videoId = searchParams.get("v");
+  const [size, setSize] = useState("max-w-[350px]");
 
   // Player options — same as YouTube IFrame API options
   const opts = {
@@ -44,10 +45,57 @@ export default function WatchPage() {
     );
   }
 
+  const handleChangeSize = (sizeKey: "s" | "m" | "l" | "t") => {
+    const sizeMap: Record<string, string> = {
+      s: "max-w-[300px]",
+      m: "max-w-[350px]",
+      l: "max-w-[500px]",
+      t: "max-w-[75%]",
+    };
+    setSize(sizeMap[sizeKey]);
+  };
+
+  const isCentered = size === "max-w-[75%]";
+
+  console.log("Size:", size);
+
   return (
-    <div className="flex flex-col items-end justify-end p-4 bg-[url(/images/bg.png)] h-[calc(100vh-72px)]">
+    <div
+      className={`
+            flex flex-col  p-4  h-[calc(100vh-72px)] bg-[url(/images/bg.png)] ${
+              isCentered
+                ? "items-center justify-center"
+                : "items-end justify-end"
+            }`}
+    >
+      <div className=" flex gap-2 mb-4">
+        <button
+          className=" bg-[#989898] rounded-sm px-2 aspect-square hover:bg-white"
+          onClick={() => handleChangeSize("s")}
+        >
+          S
+        </button>
+        <button
+          className=" bg-[#989898] rounded-sm px-2 aspect-square hover:bg-white"
+          onClick={() => handleChangeSize("m")}
+        >
+          M
+        </button>
+        <button
+          className=" bg-[#989898] rounded-sm px-2 aspect-square hover:bg-white"
+          onClick={() => handleChangeSize("l")}
+        >
+          L
+        </button>
+        <button
+          className=" bg-[#989898] rounded-sm px-2 aspect-square hover:bg-white"
+          onClick={() => handleChangeSize("t")}
+        >
+          T
+        </button>
+      </div>
       <div
-        className="relative w-full max-w-[400px]"
+        className={`relative w-full ${size}`}
         style={{ aspectRatio: "16 / 9" }}
       >
         <YouTube
